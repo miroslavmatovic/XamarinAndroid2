1909932f44e6351dc65aa38dd9916290e2fdd065
miroslavmatovic <v-mirosm@microsoft.com>
2021-05-26 12:04:40 +0200

    Xamarin.Forms

---
+++ XamarinAndroid2/XamarinAndroid2/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinAndroid2.ViewModels;
+
+namespace XamarinAndroid2.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ XamarinAndroid2/XamarinAndroid2/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinAndroid2.Models;
+using XamarinAndroid2.ViewModels;
+
+namespace XamarinAndroid2.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
